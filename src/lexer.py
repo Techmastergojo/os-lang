d9569f00eaff57d27e6059f466dad80acb9d5f7c
techmastergojo <hmzetehsin@gmail.com>
2026-06-11 12:36:15 +0500

    feat(01-03): Implement single-character scanning

--- src/lexer.py
+++ src/lexer.py
@@ -6,6 +6,50 @@ class Lexer:
         self.source = source
         self.line = 1
         self.column = 1
+        self.current = 0
+
+    def is_at_end(self) -> bool:
+        return self.current >= len(self.source)
+
+    def advance(self) -> str:
+        if self.is_at_end():
+            return '\0'
+        c = self.source[self.current]
+        self.current += 1
+        if c == '\n':
+            self.line += 1
+            self.column = 1
+        else:
+            self.column += 1
+        return c
+
+    def peek(self) -> str:
+        if self.is_at_end():
+            return '\0'
+        return self.source[self.current]
 
     def lex(self) -> List[Token]:
-        return [Token(TokenType.EOF, "", self.line, self.column)]
+        tokens = []
+        while not self.is_at_end():
+            start_col = self.column
+            c = self.advance()
+            
+            if c in [' ', '\t', '\r', '\n']:
+                continue
+            
+            token_type = None
+            if c == '+': token_type = TokenType.PLUS
+            elif c == '-': token_type = TokenType.MINUS
+            elif c == '=': token_type = TokenType.ASSIGN
+            elif c == '(': token_type = TokenType.LPAREN
+            elif c == ')': token_type = TokenType.RPAREN
+            elif c == ':': token_type = TokenType.COLON
+            elif c == ',': token_type = TokenType.COMMA
+            
+            if token_type is not None:
+                tokens.append(Token(token_type, c, self.line, start_col))
+            else:
+                pass # Unhandled characters for now
+
+        tokens.append(Token(TokenType.EOF, "", self.line, self.column))
+        return tokens
